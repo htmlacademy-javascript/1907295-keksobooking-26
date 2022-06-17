a3e5e83032e286fb81645baa72591d3a0a839b15
Екатерина <90-rina@mail.ru>
2022-06-17 12:54:30 +0300

    2.17.Нужно больше функций_3

--- js/main.js
+++ js/main.js
@@ -1,36 +1,26 @@
-function getRandomInteger(min, max) {
-  if (min < 0 || max < 0) {
+function getRandomNumber (min, max) {
+  if (min <= 0 || max <= 0) {
     throw 'Значение min и max должно быть >= 0';
   } else if (max <= min) {
     throw 'Значение max не может быть меньше или равным min';
   }
-
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  const randomNumber = Math.random() * (max - min + 1);
-  return Math.floor(randomNumber);
+  return Math.random() * (max - min + 1);
 }
 
-const randomInteger = getRandomInteger(10, 50);
+function getRandomInteger(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(getRandomNumber(min, max));
+}
+
+const randomInteger = getRandomInteger(1, 30);
 
 randomInteger();
 
-function getGeographicCoordinates(from, before) {
-  if (from < 0 || before < 0) {
-    throw 'Значение from и before должно быть >= 0';
-  } else if (before <= from) {
-    throw 'Значение before не может быть меньше или равным from';
-  }
-
-  from = Math.round(from.toFixed(1));
-  before = Math.round(before.toFixed(1));
-
-  const coordinatesX = from * Math.cos();
-  const coordinatesY = before * Math.sin();
-  return Math.random(coordinatesX.toFixed(1), coordinatesY.toFixed(1));
+function getRandomFloat(min, max, signCount) {
+  return Number(getRandomNumber(min, max).toFixed(signCount));
 }
 
-const geographicCoordinates = getGeographicCoordinates(1.1, 2.2);
+const randomFloat = Number(getRandomFloat(1.1, 2.2, 1));
 
-geographicCoordinates();
+randomFloat();
