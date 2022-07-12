5c36c785b407b99e6301073afe05deb6384be2f5
Keks <keks@htmlacademy.ru>
2022-07-12 12:29:30 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/util.js
@@ -0,0 +1,21 @@
+//возвращающая случайное целое число из переданного диапазона включительно
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+//возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+function getRandomPositiveFloat (a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+}
+
+function getRandomArrayElement (elements) {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+}
+
+export {getRandomPositiveInteger, getRandomPositiveFloat, getRandomArrayElement};
