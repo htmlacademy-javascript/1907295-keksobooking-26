5a9c8b2a96f7fd393714e79aeb663fc5d87247ac
PshenichnayaE <90998069+PshenichnayaE@users.noreply.github.com>
2022-07-21 10:26:42 +1000

    Перевод страницы в активное и неактивное состояние

--- js/card.js
+++ js/card.js
@@ -1,4 +1,4 @@
-const OFFERS_TYPES = {
+const offerTypeToTitle = {
   'palace': 'Дворец',
   'flat': 'Квартира',
   'house': 'Дом ',
@@ -16,7 +16,7 @@ function getCardElement(advert) {
   cardElement.querySelector('.popup__title').textContent = advert.offer.title;
   cardElement.querySelector('.popup__text--address').textContent = advert.offer.address;
   cardElement.querySelector('.popup__text--price').textContent = `${advert.offer.price} ₽/ночь`;
-  cardElement.querySelector('.popup__type').textContent = OFFERS_TYPES[advert.offer.type];
+  cardElement.querySelector('.popup__type').textContent = offerTypeToTitle[advert.offer.type];
   cardElement.querySelector('.popup__text--capacity').textContent = `${advert.offer.rooms} комнаты для ${advert.offer.guests} гостей`;
   cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${advert.offer.checkin}, выезд до ${advert.offer.checkout}`;
   cardElement.querySelector('.popup__description').textContent = advert.offer.description;
--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import {createOffers, DEFAULT_OFFERS_COUNT} from './data.js';
 import {getCardElement} from './card.js';
 import {getRandomArrayElement} from './util.js';
+import {setActiveAdForm} from './form.js';
 
 const offers = createOffers(DEFAULT_OFFERS_COUNT);
 const cardElement = getCardElement(getRandomArrayElement(offers));
 const mapElement = document.querySelector('.map__canvas');
 
 mapElement.appendChild(cardElement);
+setActiveAdForm();
