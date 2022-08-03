133ef39851909f855cd5c9aa9fe1a4a3eb2e22f1
Keks <keks@htmlacademy.ru>
2022-08-03 14:54:43 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,11 @@
+import {createOffers, DEFAULT_OFFERS_COUNT} from './data.js';
+import {getCardElement} from './card.js';
+import {getRandomArrayElement} from './util.js';
+import './form.js';
+import './validator.js';
+
+const offers = createOffers(DEFAULT_OFFERS_COUNT);
+const cardElement = getCardElement(getRandomArrayElement(offers));
+const mapElement = document.querySelector('.map__canvas');
+
+mapElement.appendChild(cardElement);
