2e08b0550c5a9974fa5eaafc162ddeeaf32db91b
Ekaterina <90-rina@mail.ru>
2022-10-09 21:30:22 +1000

    исправление слайдера

--- js/slider.js
+++ js/slider.js
@@ -1,20 +1,21 @@
 import {typesToPrices, typeElement, priceElement} from './validator.js';
 
 const sliderElement = document.querySelector('.ad-form__slider');
+const MIN_PRICE = 0;
+const MAX_PRICE = 100000;
+const PRICE_STEP = 1000;
 
 noUiSlider.create(sliderElement, {
   range: {
-    min: 0,
-    max: 100000,
+    min: MIN_PRICE,
+    max: MAX_PRICE,
   },
   start: typesToPrices[typeElement.value],
-  step: 1000,
+  step: PRICE_STEP,
   connect: 'lower',
   format: {
     to: function (value) {
-      if (Number.isInteger(value)) {
-        return value.toFixed(0);
-      }
+      return value.toFixed(0);
     },
     from: function (value) {
       return parseFloat(value);
@@ -22,15 +23,15 @@ noUiSlider.create(sliderElement, {
   },
 });
 
-sliderElement.noUiSlider.on('update', () => {
+sliderElement.noUiSlider.on('update', function () {
   priceElement.value = sliderElement.noUiSlider.get();
 });
 
-typeElement.addEventListener('change', () => {
+typeElement.addEventListener('change', function () {
   sliderElement.noUiSlider.updateOptions({
     range: {
-      min: 0,
-      max: 100000,
+      min: MIN_PRICE,
+      max: MAX_PRICE,
     },
     start: typesToPrices[typeElement.value],
   });
