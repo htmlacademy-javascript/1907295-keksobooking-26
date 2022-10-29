c8b1487277d4177d380a5c0f5c9b5e0d128c64d0
Ekaterina <90-rina@mail.ru>
2022-10-29 17:02:21 +1000

    добавила 2 страницу с картой

--- js/about.js
+++ js/about.js
@@ -5,8 +5,11 @@ import './slider.js';
 import {init as initMap, setMarkers} from './map.js';
 
 const offers = createOffers(DEFAULT_OFFERS_COUNT);
-const mapElement = document.querySelector('.map__canvas');
+const mapElement = document.querySelector('.map__about');
 const addressElement = document.querySelector('#address');
+
+// const COORDINATE_LAT = 55.75222;
+// const COORDINATE_LNG = 37.61556;
 
 const leafletMap = initMap(mapElement, addressElement);
 setMarkers(offers, leafletMap);
--- js/map.js
+++ js/map.js
@@ -1,77 +1,68 @@
-import {createOffers, DEFAULT_OFFERS_COUNT} from './data.js';
 import {getCardElement} from './card.js';
-import {getRandomArrayElement} from './util.js';
+import {setActiveAdForm} from './form.js';
 
-const offers = createOffers(DEFAULT_OFFERS_COUNT);
-const cardElement = getCardElement(getRandomArrayElement(offers));
-const mapElement = document.querySelector('.map__canvas');
-const addressElement = document.querySelector('#address');
+export const COORDINATE_LAT = 35.68950;
+export const COORDINATE_LNG = 139.69171;
+const ZOOM = 13;
 
-//Создание карты
-const map = L.map(mapElement)
-  .setView({
-    lat: 35.68950,
-    lng: 139.69171,
-  }, 10);
+const pinIcon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
 
-//Отображение карты
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
-).addTo(map);
-
-//Стилизация маркера
 const mainPinIcon = L.icon({
   iconUrl: './img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
 
-const PinIcon = L.icon({
-  iconUrl: './img/pin.svg',
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
-});
-
-//Добавление маркера
-const mainPinMarker = L.marker(
-  {
-    lat: 35.68950,
-    lng: 139.69171,
-  },
-  {
-    draggable: true,//перемещение метки
-    icon: mainPinIcon,
-  },
-);
-
-mainPinMarker.addTo(map);
-
-offers.forEach((offer) => {
-  const marker = L.marker(
-    {
-      lat: offer.location.lat,
-      lng: offer.location.lng,
-    },
-    {
-      icon: PinIcon,
-    }
-  );
-
-  marker
-    .addTo(map)
-    .bindPopup(cardElement);
-});
-
-//Записываем новые координаты при передвижении метки
-function getAddress ({lat, lng}) {
+// Получение координат
+function getAddress(addressElement, {lat, lng}) {
   addressElement.value = `${lat.toFixed(5)}, ${lng.toFixed(5)}`;
 }
 
-mainPinMarker.on('moveend', (evt) => {
-  getAddress(evt.target.getLatLng());
-});
+// Добавление марки и балуна
+export function setMarkers(offers, markerGroup) {
+  offers.forEach((offer) => {
+    const cardElement = getCardElement(offer);
+    const marker = L.marker(
+      { lat: offer.location.lat, lng: offer.location.lng },
+      { icon: pinIcon }
+    );
 
-// mapElement.appendChild(cardElement);
+    marker
+      .addTo(markerGroup)
+      .bindPopup(cardElement);
+  });
+}
+
+// Отображение карты
+export function init(mapElement, addressElement) {
+  const map = L.map(mapElement)
+    .on('load', () => {
+      setActiveAdForm(true);
+    })
+    .setView({ lat: COORDINATE_LAT, lng: COORDINATE_LNG }, ZOOM);
+
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
+
+  const mainPinMarker = L.marker(
+    { lat: COORDINATE_LAT, lng: COORDINATE_LNG },
+    { draggable: true, icon: mainPinIcon, },
+  );
+
+  const markerGroup = L.layerGroup().addTo(map);
+  mainPinMarker.addTo(markerGroup);
+
+  mainPinMarker.on('moveend', ({target}) => {
+    getAddress(addressElement, target.getLatLng());
+  });
+
+  return map;
+}
